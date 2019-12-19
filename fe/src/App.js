632d154460bed2c18cef588d2dac0c3d1a1a240c
Paul M Edwards <pauledwards@gmail.com>
2019-12-18 20:45:09 -0800

    Login.
    Co-authored-by: VitaliyM3 <Vitaliymikhaylyuk@gmail.com>

--- fe/src/App.js
+++ fe/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import PrivateRoute from './components/PrivateRoute';
-// import Login from './components/Login';
+import Login from './components/Login';
 
 import './App.scss';
 
@@ -10,8 +10,8 @@ function App() {
   return (
     <Router>
       <div className="App">
-        {/* <Route exact path="/" component={Login} />
-        <PrivateRoute exact path="/tbd" component={TBD} /> */}
+        <Route exact path="/" component={Login} />
+        {/* <PrivateRoute exact path="/tbd" component={TBD} /> */}
       </div>
     </Router>
   );
