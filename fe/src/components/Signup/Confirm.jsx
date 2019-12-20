f1440c5cdb11b787ed573c2dd10afa9d4d86e44b
Aaron-Doran <wesley.gossett@gmail.com>
2019-12-19 19:15:42 -0500

    STarted on signup form need to add validation and styling to the project.

---
+++ fe/src/components/Signup/Confirm.jsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import moduleName from '@material-ui/core/Paper';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
+
+const useStyles = makeStyles({
+    root: {
+        padding: '3px 2px',
+    },
+});
+
+const Confirm = props => {
+    const classes = useStyles();
+    const next = e => {
+        e.preventDefault();
+        props.nextStep();
+    }
+
+    const back = e => {
+        e.preventDefault();
+        props.prevStep();
+    }
+    const { values } = props;
+    return (
+        <Paper className={classes.root}>
+            <Typography variant='h5' component='h2'>
+                {values.email}
+            </Typography>
+            <Typography variant='h5' component='h2'>
+                {values.userName}    
+            </Typography>
+            <Typography variant='h5' component='h2'>
+                {values.password}    
+            </Typography>
+            <br/>
+            <Button
+                margin='normal'
+                halfWidth
+                variant='contained'
+                label='Continue'
+                style={styles.button}
+                onClick={next}>
+                    Confirm & Continue        
+            </Button>
+            <Button
+                margin='normal'
+                halfWidth
+                variant='contained'
+                label='Back'
+                style={styles.button}
+                onClick={back}>
+                Go Back
+            </Button>    
+        </Paper>
+    )
+}
+
+const styles = {
+    button: {
+        margin: 15
+    }
+}
+
+export default Confirm;
---
+++ fe/src/components/Signup/FormUserDetails.jsx
@@ -0,0 +1,76 @@
+import React from 'react';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import Container from '@material-ui/core/Container';
+import { StylesContext } from '@material-ui/styles';
+
+
+const FormUserDetails = props => {
+    const next = e => {
+        e.preventDefault();
+        props.nextStep();
+        props.onSubmit(e);
+    }
+
+    const {values, handleChanges, handleChecked} = props;
+
+    return (
+        <Container>
+            <TextField
+                helperText={values.email}
+                onChange={handleChanges('email')}
+                defaultValue={values.email}
+                variant='outlined'
+                margin='normal'
+                required
+                halfWidth
+                name='email'
+                label='Email'
+            />
+            <TextField
+                helperText={values.userNameError}
+                onChange={handleChanges('userName')}
+                defaultValue={values.userName}
+                variant='outlined'
+                margin='normal'
+                required
+                halfWidth
+                name='userName'
+                label='User Name'
+            />
+            <br/>
+            <TextField
+                onChange={handleChanges('password')}
+                defaultValue={values.password}
+                variant='outlined'
+                margin='normal'
+                required
+                halfWidth
+                name='password'
+                label='password'
+                type='password'
+                id='password'
+                autoComplete='current-password' 
+                />
+                <Button
+                    type='submit'
+                    onSubmit={props.onSubmit}
+                    margin='normal'
+                    halfWidth
+                    variant='contained'
+                    label='Continue'
+                    style={styles.button}
+                    onClick={next}
+                >Continue
+                </Button>
+                </Container>
+    )
+}
+
+const styles = {
+    button: {
+        margin: 15
+    }
+}
+
+export default FormUserDetails;
