ebd1d2d19cd23aaed54b8f079e69b1be6ba56008
Aaron-Doran <wesley.gossett@gmail.com>
2020-01-07 22:23:05 -0500

    Made chage to profile in App.js had it as Route switched it back to PrivateRoute.

--- fe/src/App.js
+++ fe/src/App.js
@@ -20,7 +20,7 @@ function App() {
         <Route exact path="/" component={Welcome} />
         <Route exact path="/login" component={Login} />
         <Route exact path='/signup' component={UserForm} />
-        <Route exact path="/profile" component={Profile} />
+        <PrivateRoute exact path="/profile" component={Profile} />
       </div>
     </Router>
   );
